c0db9d0b15e9947c86f39ea64406d5285dbd9d01
durgabhemesh <114475114+durgabhemesh@users.noreply.github.com>
2022-10-07 08:21:26 +0530

    Updates

---
+++ precache-manifest.23aadf51d7bd32c111e45dfc1a3f6a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce89c28d20aed038edaa",
+    "url": "/my-app/static/css/main.5537ebfd.chunk.css"
+  },
+  {
+    "revision": "ce89c28d20aed038edaa",
+    "url": "/my-app/static/js/main.ce89c28d.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/my-app/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "fca5637eeba2b44463e9",
+    "url": "/my-app/static/js/2.fca5637e.chunk.js"
+  },
+  {
+    "revision": "dd5f09622e4867b76f52",
+    "url": "/my-app/static/js/runtime~main.dd5f0962.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9050770203145f1f434d3c4dd84c1d6e",
+    "url": "/my-app/index.html"
+  }
+];
